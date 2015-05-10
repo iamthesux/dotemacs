f79374df0c653e8c18de77ba811656a1fd35e808
iamthesux <iamthesux@gmail.com>
2015-05-10 17:37:12 -0400

    message buffer tails

--- tools.el
+++ tools.el
@@ -26,3 +26,15 @@
   (mapcar (lambda (x) (kill-buffer x))
 	  (buffer-list))
   (delete-other-windows))
+
+;; makes the Messages buffer behave like tail
+;; http://stackoverflow.com/a/4685005
+(defadvice message (after message-tail activate)
+  "goto point max after a message"
+  (with-current-buffer "*Messages*"
+    (goto-char (point-max))
+    (walk-windows (lambda (window)
+                    (if (string-equal (buffer-name (window-buffer window)) "*Messages*")
+                        (set-window-point window (point-max))))
+                  nil
+                  t)))
