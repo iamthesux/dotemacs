bbbb3087ec4819526d4e29fa0cd76d04b589515f
iamthesux <iamthesux@gmail.com>
2015-05-10 18:04:23 -0400

    magit status keybind

--- keybinds.el
+++ keybinds.el
@@ -4,6 +4,7 @@
 (global-set-key (kbd "C-c c") 'comment-or-uncomment-region)
 (global-set-key (kbd "<C-tab>") 'other-window)
 (global-set-key (kbd "C-c C-r") 'replace-regexp)
+(global-set-key (kbd "C-c g") 'magit-status)
 
 ;; TODO get this to work
 ;; (define-key persp-key-map (kbd "C-x x") 'persp-keymap-prefix)
@@ -11,4 +12,3 @@
 (with-eval-after-load "help-mode"
   (define-key help-mode-map (kbd "<mouse-5>") 'help-go-forward)
   (define-key help-mode-map (kbd "<mouse-4>") 'help-go-back))
-
